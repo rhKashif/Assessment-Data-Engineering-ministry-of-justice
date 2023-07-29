0a4d08fd0965b0be063e67371dc199e943317848
Hassan Kashif <hassankashif@Hassans-MacBook-Pro.local>
2023-07-29 15:45:44 +0100

    implemented get_court_by_postcode get_people_from_csv

--- test_2.py
+++ test_2.py
@@ -1,3 +1,5 @@
+"""test_2 - the second test as part of the ministry of justice take home code assessment"""
+
 # A team of analysts wish to discover how far people are travelling to their nearest
 # desired court. We have provided you with a small test dataset so you can find out if
 # it is possible to give the analysts the data they need to do this. The data is in
@@ -56,7 +58,7 @@
         "distance": 1.29
     },
     etc
-]
+
 """
 
 # Use this API and the data in people.csv to determine how far each person's nearest
@@ -69,6 +71,99 @@
 # - the dx_number (if available) of the nearest court of the right type
 # - the distance to the nearest court of the right type
 
+
+
+
+import os
+import glob
+import requests
+import pandas as pd
+class APIError(Exception):
+    """Describes an error triggered by a failing API call."""
+
+    def __init__(self, message: str, code: int = 500):
+        """Creates a new APIError instance."""
+        self.message = message
+        self.code = code
+
+
+def get_court_by_postcode(postcode: str) -> list[dict]:
+    """
+    Given a postcode, returns a list of the 10 nearest courts
+
+    Argument:
+        postcode (str): A string representing the postcode.
+
+    Returns:
+        list: a list of dicts containing information associated with the court
+
+    Raises:
+        TypeError: If the input argument is not of type str.
+
+    """
+    if not isinstance(postcode, str):
+        raise TypeError("Input argument must be in str format!")
+    url = f'https://courttribunalfinder.service.gov.uk/search/results.json?postcode={postcode}'
+    response = requests.get(
+        f'{url}', timeout=60)
+    if response.status_code != 200:
+        raise APIError(f'API Response:{response.status_code}')
+
+    data = response.json()
+    return data
+
+
+def get_people_from_csv(csv_file_name: str) -> list[dict]:
+    """
+    Given the people csv file, returns a list of people data
+
+    Argument:
+        csv_file_name (str): A string representing the file name (not including file extension).
+
+    Returns:
+        list: a list of dicts containing information associated with a person
+
+    Raises:
+        TypeError: If the input argument is not of type str.
+
+    """
+
+    current_directory = os.getcwd()
+    csv_file_path = os.path.join(current_directory, f'{csv_file_name}.csv')
+    file = glob.glob(csv_file_path)[0]
+    data = pd.read_csv(file).to_numpy()
+    csv_data = []
+    for entry in data:
+        csv_data_entry = {}
+        csv_data_entry["person_name"] = entry[0]
+        csv_data_entry["home_postcode"] = entry[1]
+        csv_data_entry["looking_for_court_type"] = entry[2]
+        csv_data.append(csv_data_entry)
+    return csv_data
+
+
+def get_courts_for_person(people_data: list[dict]):
+    """"""
+    for person in people_data:
+        name = person["person_name"]
+        postcode = person["home_postcode"]
+        desired_type = person["looking_for_court_type"]
+
+        court_data = get_court_by_postcode(postcode)
+        for court in court_data:
+            try:
+                court_type = court["types"][0]
+            except:
+                court_type = None
+            # print(desired_type, court_type)
+            if desired_type == court_type:
+                print(court)
+            break
+        break
+
+
 if __name__ == "__main__":
-    # [TODO]: write your answer here
-    pass
+    postcode = 'E144PU'
+    csv_file_name = 'people'
+    people_data = get_people_from_csv(csv_file_name)
+    res = get_courts_for_person(people_data)
---
+++ test_test_2.py
@@ -0,0 +1,6 @@
+"""tests for functions defined in test_2.py"""
+# pylint: skip-file
+import pytest
+from unittest.mock import patch
+
+import test_2
